e9d40bfc8ee4723613109ce39aa7b08c2e42bf3e
Benzz <ben.ribeiro9@gmail.com>
2024-09-25 05:47:41 +0200

    etape 1.3 fini

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const userRoutes = require('./routes/user');
+
+mongoose
+  .connect('mongodb+srv://test:portugal9@coursoc.e6b5f.mongodb.net/CoursOC?retryWrites=true&w=majority')
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 const app = express();
 
 app.use((req, res, next) => {
-  console.log('Requête reçue !');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
 });
 
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
+app.use(express.json());
 
-app.use((req, res, next) => {
-  res.json({ message: 'Votre requête à bien été reçue !' });
-  next();
-});
-
-app.use((req, res) => {
-  console.log('Reponse envoyé');
-});
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
